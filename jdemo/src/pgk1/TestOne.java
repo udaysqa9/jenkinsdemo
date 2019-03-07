a5e2ba882bce838be25f771a298f74562da35f73
uday <uday.sqa9@gmail.com>
2019-03-07 12:20:49 +0530

    Updated java files

---
+++ jdemo/src/pgk1/TestOne.java
@@ -0,0 +1,15 @@
+package pgk1;
+
+public class TestOne {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test Runner");
+		System.out.println("Test Runner");
+		System.out.println("Test Runner");
+		System.out.println("Test Runner");
+		System.out.println("Test Runner");
+		
+	}
+}
